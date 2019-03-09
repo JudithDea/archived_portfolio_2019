a5dd36e05ad1b5ea5e0659833f19fe28ef38a46f
Judith Dea <JudithFoe@gmail.com>
2019-03-08 22:49:23 -0500

    added new skills text fadin in / out feature

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ $(function() {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
+
+// Awesome new skills feature
+
+(function() {
+  var newSkills = $(".newSkills");
+  var skillsIndex = -1;
+
+  function showNextSkill() {
+    ++skillsIndex;
+    newSkills
+      .eq(skillsIndex % newSkills.length)
+      .fadeIn(1500)
+      .delay(1500)
+      .fadeOut(1500, showNextSkill);
+  }
+
+  showNextSkill();
+})();
